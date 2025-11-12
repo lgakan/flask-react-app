9015fca8b46198d07144502d03d7ac383ff4bcae
lukgakan <lukgakan@gmail.com>
2025-11-12 13:17:13 +0100

    Imrpoving token handling

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,6 @@ def login():
 @app.route('/refresh', methods=['POST'])
 @jwt_required(refresh=True)
 def refresh():
-    """Refreshes an expired access token."""
     current_user_id = get_jwt_identity()
     new_access_token = create_access_token(identity=current_user_id)
     return jsonify(accessToken=new_access_token), 200
